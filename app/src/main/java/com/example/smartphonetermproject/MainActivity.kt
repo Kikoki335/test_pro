1a6acd43edc38baacb326f024a151daf224233a1
kmw7855 <rrr7538@naver.com>
2026-04-10 23:32:10 +0900

    신 넘어가기

--- app/src/main/java/com/example/smartphonetermproject/MainActivity.kt
+++ app/src/main/java/com/example/smartphonetermproject/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.smartphonetermproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.smartphonetermproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+    }
+
+    fun onStartGameClicked(view: View) {
+        startGameActivity()
+    }
+
+    private fun startGameActivity() {
+        Log.d(javaClass.simpleName, "Start Game")
+        val intent = Intent(this, SkyBlasterActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/smartphonetermproject/SkyBlasterActivity.kt
@@ -0,0 +1,15 @@
+package com.example.smartphonetermproject
+
+import kr.ac.tukorea.ge.spgp2026.a2dg.activity.BaseGameActivity
+import kr.ac.tukorea.ge.spgp2026.a2dg.scene.Scene
+import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
+
+class SkyBlasterActivity : BaseGameActivity() {
+    override val drawsDebugGrid = false
+    override val drawsDebugInfo = BuildConfig.DEBUG
+    override val drawsFpsGraph = false
+
+    override fun createRootScene(gctx: GameContext): Scene {
+        return MainScene(gctx)
+    }
+}
